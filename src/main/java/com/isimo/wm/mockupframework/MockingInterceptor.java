9ead3c8d4907beda61589af7b64b84c2a381e336
Andrzej Dmoch <95031588@mail.schwarz>
2021-05-05 22:45:36 +0200

    First working version

--- src/main/java/com/isimo/wm/mockupframework/MockingInterceptor.java
+++ src/main/java/com/isimo/wm/mockupframework/MockingInterceptor.java
@@ -9,6 +9,7 @@ import com.wm.app.b2b.server.invoke.ServiceStatus;
 import com.wm.data.IData;
 import com.wm.lang.ns.NSName;
 import com.wm.lang.ns.NSNode;
+import com.wm.lang.ns.NSService;
 import com.wm.lang.ns.NSType;
 import com.wm.lang.ns.Namespace;
 import com.wm.util.Config;
@@ -25,17 +26,20 @@ public class MockingInterceptor implements InvokeChainProcessor {
 			ServiceStatus status) throws ServerException {
 		// read the invoke manager extended setting
 		String profilesStr = Config.getProperty("watt.server.mockinginterceptor.profiles");
-		if(profilesStr==null)
+		if(profilesStr==null || profilesStr.isEmpty()) {
 			continueToNext(chain, service, pipeline, status);
+			return;
+		}
 		String[] profiles = profilesStr.split(",");
 		NSName ns = service.getNSName();
 		Namespace namespace = service.getNamespace();
 		for(String profile: profiles) {
 			NSName mockCandidateNS = fromBaseName(ns, profile);
 			NSNode mockCandidateNode = namespace.getNode(mockCandidateNS);
-			if(mockCandidateNode != null && mockCandidateNode.getNodeTypeObj().equals(SERVICE_TYPE)) {
-				service.setNSName(mockCandidateNS);
+			if(mockCandidateNode != null && mockCandidateNode.getNodeTypeObj().equals(NSService.TYPE)) {
+				service = (BaseService) mockCandidateNode;
 				log("Service "+ns.getFullName()+" mocked by "+mockCandidateNS.getFullName());
+				break;
 			}
 		}
 		continueToNext(chain, service, pipeline, status);
@@ -47,7 +51,7 @@ public class MockingInterceptor implements InvokeChainProcessor {
 	}
 	
 	static void log(String msg) {
-		JournalLogger.logInfo(0, JournalLogger.FAC_SERVICE, msg);
+		JournalLogger.log(3, 90, JournalLogger.INFO, msg);
 	}
 	
 	void continueToNext(Iterator chain, BaseService service, IData pipeline,
