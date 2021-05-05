52cf6338614efbcfa0c94af8fcc2b18137f9ea97
Andrzej Dmoch <andrzej.dmoch@gmail.com>
2021-05-05 21:41:52 +0200

    Corrections

--- src/main/java/com/isimo/wm/mockupframework/MockingInterceptor.java
+++ src/main/java/com/isimo/wm/mockupframework/MockingInterceptor.java
@@ -15,27 +15,44 @@ import com.wm.util.Config;
 import com.wm.util.JournalLogger;
 import com.wm.util.ServerException;
 
-public class MockingInterceptor implements InvokeChainProcessor {
+public class MockingInterceptor implements InvokeChainProcessor {	
+	public MockingInterceptor() {
+		log("Mocking Interceptor initialized");
+	}
+	
 	static NSType SERVICE_TYPE = NSType.create("Service");
-	public void process(Iterator paramIterator, BaseService paramBaseService, IData paramIData,
-			ServiceStatus paramServiceStatus) throws ServerException {
+	public void process(Iterator chain, BaseService service, IData pipeline,
+			ServiceStatus status) throws ServerException {
 		// read the invoke manager extended setting
 		String profilesStr = Config.getProperty("watt.server.mockinginterceptor.profiles");
+		if(profilesStr==null)
+			continueToNext(chain, service, pipeline, status);
 		String[] profiles = profilesStr.split(",");
-		NSName ns = paramBaseService.getNSName();
-		Namespace namespace = paramBaseService.getNamespace();
+		NSName ns = service.getNSName();
+		Namespace namespace = service.getNamespace();
 		for(String profile: profiles) {
 			NSName mockCandidateNS = fromBaseName(ns, profile);
 			NSNode mockCandidateNode = namespace.getNode(mockCandidateNS);
 			if(mockCandidateNode != null && mockCandidateNode.getNodeTypeObj().equals(SERVICE_TYPE)) {
-				paramBaseService.setNSName(mockCandidateNS);
-				JournalLogger.logInfo(0, JournalLogger.FAC_SERVICE, "Service "+ns.getFullName()+" mocked by "+mockCandidateNS.getFullName());
+				service.setNSName(mockCandidateNS);
+				log("Service "+ns.getFullName()+" mocked by "+mockCandidateNS.getFullName());
 			}
 		}
+		continueToNext(chain, service, pipeline, status);
 	}
 	
 	NSName fromBaseName(NSName baseName, String profile) {
 		NSName retval = NSName.create(baseName.getInterfaceName()+".mockup_"+profile, baseName.getNodeName().toString());
 		return retval;
+	}
+	
+	static void log(String msg) {
+		JournalLogger.logInfo(0, JournalLogger.FAC_SERVICE, msg);
+	}
+	
+	void continueToNext(Iterator chain, BaseService service, IData pipeline,
+			ServiceStatus status) throws ServerException {
+		if (chain.hasNext())
+			((InvokeChainProcessor)chain.next()).process(chain, service, pipeline, status);
 	}
 }
